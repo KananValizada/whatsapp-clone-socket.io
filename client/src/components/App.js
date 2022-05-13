e4a837c7decd598887e5ea1ee99287c99e370277
KananValizada <kenan.veli@yahoo.com>
2022-05-13 23:58:22 +0400

    contacts functionality modals

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,18 @@
 import Login from "./Login";
 import Dashboard from "./Dashboard";
 import useLocalStorage from "../hooks/useLocalStorage";
+import { ContactsProvider } from "../context/ContactsProvider";
 
 const App = () => {
   const [id, setId] = useLocalStorage("id");
 
-  return id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />;
+  const dashboard = (
+    <ContactsProvider>
+      <Dashboard id={id} />
+    </ContactsProvider>
+  );
+
+  return id ? dashboard : <Login onIdSubmit={setId} />;
 };
 
 export default App;
--- client/src/components/NewContactModal.js
+++ client/src/components/NewContactModal.js
@@ -1,5 +1,37 @@
-import React from "react";
+import { useRef } from "react";
+import { Button, Form, Modal } from "react-bootstrap";
+import { useContacts } from "../context/ContactsProvider";
 
-export default function NewContactModal() {
-  return <div>NewContactModal</div>;
+export default function NewContactModal({ closeModal }) {
+  const idRef = useRef();
+  const nameRef = useRef();
+  const { createContact } = useContacts();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    createContact(idRef.current.value, nameRef.current.value);
+    closeModal();
+  };
+
+  return (
+    <>
+      <Modal.Header>Create Contact</Modal.Header>
+      <Modal.Body>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group>
+            <Form.Label>Id</Form.Label>
+            <Form.Control type="text" ref={idRef} required />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="text" ref={nameRef} required />
+          </Form.Group>
+          <Button type="submit" className="mt-2">
+            Create
+          </Button>
+        </Form>
+      </Modal.Body>
+    </>
+  );
 }
